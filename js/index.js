4639c8ba06152fb8dfdba2030795f59075ca06fc
Ericksonprofissional <ericksonprofissional@gmail.com>
2020-02-27 13:01:19 -0400

    Operadores logicos e validaçao do peso ou alturas invalidas

--- js/index.js
+++ js/index.js
@@ -1,13 +1,29 @@
 var titulo = document.querySelector(".titulo");
 titulo.innerText = "Erickson Nutricionita";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
+var trPaciente = document.querySelector("#primeiro-paciente");
+var tdPeso = trPaciente.querySelector(".info-peso");
 var peso = tdPeso.innerHTML;
 
-var tdAltura = paciente.querySelector(".info-altura");
+var tdAltura = trPaciente.querySelector(".info-altura");
 var altura = tdAltura.innerHTML;
 
-var imc = peso / (altura * altura);
-var tdImc =  paciente.querySelector(".info-imc");
-tdImc.innerHTML = imc;
+var tdImc =  trPaciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso < 1 || peso > 400){
+    pesoEhValido = false;
+    tdImc.innerHTML = "Peso invalido";
+}
+
+if(altura < 0.1 || altura > 3){
+    alturaEhValida = false;
+    tdImc.innerHTML = "Altura invalida";
+}
+if( alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.innerHTML = imc;
+}
+
