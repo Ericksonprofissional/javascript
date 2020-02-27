3b69411e4a15f900adea3689aa8105dcda868b95
Ericksonprofissional <ericksonprofissional@gmail.com>
2020-02-27 13:20:36 -0400

    Replicando a validaçao e o calculo do IMC para todos os pacientes

--- js/index.js
+++ js/index.js
@@ -1,29 +1,34 @@
 var titulo = document.querySelector(".titulo");
 titulo.innerText = "Erickson Nutricionita";
 
-var trPaciente = document.querySelector("#primeiro-paciente");
-var tdPeso = trPaciente.querySelector(".info-peso");
-var peso = tdPeso.innerHTML;
 
-var tdAltura = trPaciente.querySelector(".info-altura");
-var altura = tdAltura.innerHTML;
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdImc =  trPaciente.querySelector(".info-imc");
+for(var i = 0;i < pacientes.length;i++){
+    trPaciente = pacientes[i];
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var tdPeso = trPaciente.querySelector(".info-peso");
+    var peso = tdPeso.innerHTML;
 
-if(peso < 1 || peso > 400){
-    pesoEhValido = false;
-    tdImc.innerHTML = "Peso invalido";
+    var tdAltura = trPaciente.querySelector(".info-altura");
+    var altura = tdAltura.innerHTML;
+
+    var tdImc =  trPaciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if(peso < 1 || peso > 400){
+        pesoEhValido = false;
+        tdImc.innerHTML = "Peso invalido";
+    }
+
+    if(altura < 0.1 || altura > 3){
+        alturaEhValida = false;
+        tdImc.innerHTML = "Altura invalida";
+    }
+    if( alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.innerHTML = imc.toFixed(2);
+    }
 }
-
-if(altura < 0.1 || altura > 3){
-    alturaEhValida = false;
-    tdImc.innerHTML = "Altura invalida";
-}
-if( alturaEhValida && pesoEhValido){
-    var imc = peso / (altura * altura);
-    tdImc.innerHTML = imc;
-}
-
