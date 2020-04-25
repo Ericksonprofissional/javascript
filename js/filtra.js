1b75dc0f6fed798bf234aa12f044132ddbcef3c2
ericksonprofissional <ericksonprofissional@gmail.com>
2020-04-25 09:12:35 -0400

    Implementando a lógica de filtragem

---
+++ js/filtra.js
@@ -0,0 +1,18 @@
+let filtrando = document.querySelector("#filtro-tabela");
+
+filtrando.addEventListener("input",(el)=>{
+    let pesquisaNome = (el.target.value);
+    let pacientes = document.querySelectorAll(".paciente");
+
+    if(pesquisaNome.length > 0 ){
+        pacientes.forEach(paciente => {
+            let nomePaciente = paciente.querySelector(".info-nome").innerHTML;
+            pesquisaNome == nomePaciente ? paciente.classList.remove("invisivel") : paciente.classList.add("invisivel");
+        });
+    }else{
+        pacientes.forEach(paciente => { 
+            paciente.classList.remove("invisivel");
+        });
+    }
+    
+})
