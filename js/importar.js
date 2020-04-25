4a6a078f11c94a52d9dae42baa4cd09ec69e03b3
ericksonprofissional <ericksonprofissional@gmail.com>
2020-04-25 12:54:57 -0400

    Capturando erros de requisição

--- js/importar.js
+++ js/importar.js
@@ -1,14 +1,16 @@
 let importarPaciente = document.querySelector("#importar-paciente");
-
 importarPaciente.addEventListener("click", () => {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     xhr.send();
     xhr.addEventListener("load", () => {
-
-        let pacientes = JSON.parse(xhr.responseText);
-        pacientes.forEach(paciente => {
-            adicionaPacienteTabela(paciente);
-        });
+        if (xhr.status == 200) {
+            let pacientes = JSON.parse(xhr.responseText);
+            pacientes.forEach(paciente => {
+                adicionaPacienteTabela(paciente);
+            });
+        }else{
+            document.querySelector("#erro-import-paciente").classList.remove("invisivel");
+        }
     });
 });
